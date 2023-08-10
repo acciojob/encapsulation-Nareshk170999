d14423f64c9574019ddbe168197425fb5443831f
nares <naresh170999@gmail.com>
2023-08-10 16:13:16 +0530

    submit1

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -6,7 +6,7 @@ public class RWOnly {
     public String getName(){
         return name;
     }
-    public void setName(String name)[
+    public void setName(String name){
     this.name = name;
-}
+    }
 }
